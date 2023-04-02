f3e5e261b9071ee6edd80bdd808b83f5b3d9c09b
KevinBett <kevinbett458@gmail.com>
2023-04-02 23:50:38 +0300

    generate the movie details of the the movie on id 1

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,3 +22,29 @@ const fetchFilms = () => {
     console.log(error);
   });
 }
+
+// GET request to retrieve first movie data from the API
+fetch(`${BASE_URL}/films/1`)
+  .then(response => response.json())
+  .then(movieData => {
+    // Show available tickets from the theater's capacity and the number of tickets sold
+    const availableTickets = movieData.capacity - movieData.tickets_sold;
+
+    // Use the HTML elements to display the movie details
+    const posterImage = document.getElementById('poster-img');
+    const movieTitle = document.getElementById('movie-title');
+    const movieRuntime = document.getElementById('movie-runtime');
+    const movieShowtime = document.getElementById('movie-showtime');
+    const movieTickets = document.getElementById('available-tickets');
+
+    // Set the text and image content of the HTML elements using the movie data
+    posterImage.src = movieData.poster;
+    movieTitle.textContent = movieData.title;
+    movieRuntime.textContent = `${movieData.runtime} minutes`;
+    movieShowtime.textContent = movieData.showtime;
+    movieTickets.textContent = `${availableTickets} tickets available`;
+  })
+  .catch(error => {
+    console.error('Cannot fetch movie details:', error);
+  });
+
